413350606a91d2e45cffb0f10ead1eaa1c61a94d
Edson <craes20@hotmail.com>
2024-06-14 16:03:43 -0600

    no se que pedo

---
+++ Presentation/Controllers/PacientesController.cs
@@ -0,0 +1,28 @@
+﻿using Core.Features.Pacientes.Command;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[ApiController]
+[Route("[controller]")]
+
+public class PacientesController: ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public PacientesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost("CrearPaciente")]
+    public async Task<IActionResult> PostPaciente([FromBody] CrearPaciente command)
+    {
+        await _mediator.Send(command);
+        return Ok("El paciente se registro correctamente");
+    }
+}
--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e759f2d420ac81c584a6e1e59991ad523c2f8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d9dc8be9408169f78307548467532a86585e01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
