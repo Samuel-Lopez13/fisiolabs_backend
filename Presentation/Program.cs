38355759ce1c5fda32297cb923ae193343ad1454
Samuel-Lopez13 <lopez180219@gmail.com>
2024-06-15 11:11:08 -0600

    prueba 20

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("http://localhost:5173/Pacientes/AgregarPaciente")
+            builder.WithOrigins("http://localhost:5173", "http://localhost:5173/Pacientes/AgregarPaciente")
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
