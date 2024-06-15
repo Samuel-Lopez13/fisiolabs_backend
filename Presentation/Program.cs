d6fcc4935d7f926de1cf055643ee6beb996a165c
Samuel-Lopez13 <lopez180219@gmail.com>
2024-06-15 10:11:26 -0600

    Otra pruebaa

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -16,10 +16,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("http://localhost:5173")
+            builder.WithOrigins("http://localhost:5173/")
                 .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials();
+                .AllowAnyMethod();
         });
 });
 builder.Services.AddControllers();
