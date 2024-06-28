38150adebf0bb870db464230ddf74cb7d013219c
Samuel-Lopez13 <lopez180219@gmail.com>
2024-06-28 14:56:56 -0600

    Buscador y Pacientes

--- Core/Features/Pacientes/queries/BuscarPaciente.cs
+++ Core/Features/Pacientes/queries/BuscarPaciente.cs
@@ -1,6 +1,8 @@
-﻿using Core.Infraestructure.Persistance;
+﻿using Core.Domain.Helpers;
+using Core.Infraestructure.Persistance;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Core.Features.Pacientes.queries;
 
@@ -30,56 +32,69 @@ public class BuscarPacientesHandler : IRequestHandler<BuscarPaciente, BuscarPaci
         // Calcular el número de páginas
         int numPaginas = (int)Math.Ceiling((double)totalPacientes.Count / 10);
         
-        var pacientes = await _context.Pacientes
+        var pacientesList = await _context.Pacientes
             .AsNoTracking()
             .Where(x => x.Nombre.Contains(request.Nombre))
             .Include(x => x.Expedientes)
-            .OrderBy(o => o.Nombre)
+            .ToListAsync();
+
+        // Ordenar los pacientes según cada letra en la cadena de búsqueda
+        pacientesList = pacientesList
+            .OrderBy(p => 
+            {
+                //Convertimos el nombre a minuscular
+                string nombre = p.Nombre.ToLower();
+                //Esto es lo que estamos buscando
+                string criterio = request.Nombre.ToLower();
+                //Obtener los índices de cada letra en el nombre
+                int[] indices = new int[criterio.Length];
+                
+                for (int i = 0; i < criterio.Length; i++)
+                {
+                    indices[i] = nombre.IndexOf(criterio[i]);
+                    if (indices[i] == -1)
+                    {
+                        indices[i] = int.MaxValue;
+                    }
+                }
+
+                // Convertir los índices a una cadena para usarla en la comparación
+                return string.Join(",", indices);
+            })
+            .ThenBy(p => p.Nombre)
+            .ToList();
+
+        // Aplicar paginación
+        var pacientes = pacientesList
             .Skip((request.Pagina - 1) * 10)
             .Take(10)
             .Select(x => new PacientesModel()
             {
                 PacienteId = x.PacienteId,
                 Nombre = x.Nombre + " " + (x.Apellido ?? ""),
-                Edad = EdadPaciente(x.Edad.Date),
+                Edad = ConvertDate.DateToYear(x.Edad.Date),
                 Sexo = x.Sexo == true ? "Hombre" : "Mujer",
                 Telefono = x.Telefono,
                 Verificado = x.Expedientes.Any()
-            }).ToListAsync();
+            })
+            .ToList();
         
-        // Crear el response
+        // Response
         var response = new BuscarPacienteResponse()
         {
             numPaginas = numPaginas,
+            total = totalPacientes.Count,
             pacientes = pacientes
         };
 
         return response;
-    }
-
-    public static int EdadPaciente(DateTime fecha)
-    {
-        int edad = DateTime.Today.Year - fecha.Year;
-        var restar = false;
-        
-        if (DateTime.Today.Month < fecha.Month)
-            restar = true;
-        else if (DateTime.Today.Month == fecha.Month)
-        {
-            if (DateTime.Today.Day <= fecha.Day)
-                restar = true;
-        }
-
-        if (restar)
-            edad -= 1;
-
-        return edad;
     }
 }
 
 public record BuscarPacienteResponse
 {
     public int numPaginas { get; set; }
+    public int total { get; set; }
     public List<PacientesModel> pacientes { get; set; }
 }
 
