0a89f8433fd223093a593ece4ac43a9d83201747
Samuel-Lopez <al059401@uacam.com>
2024-07-09 15:54:05 -0600

    prueba 15 (Con chance esta si funcione)

--- Core/Infraestructure/Persistance/Services/Date.cs
+++ Core/Infraestructure/Persistance/Services/Date.cs
@@ -28,7 +28,8 @@ public class Date : IDate
         DateTime utcNow = DateTime.UtcNow;
         
         // Obtener la hora local en Campeche (Central Standard Time)
-        TimeZoneInfo campecheTimeZone = TimeZoneInfo.FindSystemTimeZoneById("America/Mexico_City");
+        //TimeZoneInfo campecheTimeZone = TimeZoneInfo.FindSystemTimeZoneById("America/Mexico_City");
+        TimeZoneInfo campecheTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
         DateTime campecheTime = TimeZoneInfo.ConvertTimeFromUtc(utcNow, campecheTimeZone);
         
         foreach (var cita in citas)
@@ -41,7 +42,7 @@ public class Date : IDate
                     await _context.SaveChangesAsync();
                 }
                 
-                if(cita.Fecha.Date < campecheTime){
+                if(cita.Fecha.Date < campecheTime.Date){
                     cita.Status = 2;
                     await _context.SaveChangesAsync();
                 }
